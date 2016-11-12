762367f374ba23d92bf3af7a36e30216e7a20371
Nasa Iwai <nasaiwai@gmail.com>
2016-11-12 14:22:31 -0800

    added streak function, deleted accomplishment

--- Profile.cs
+++ Profile.cs
@@ -11,17 +11,23 @@ using System.Text.RegularExpressions;
 
 public class Profile : MonoBehaviour {
 
+	/* To get data from database and display */
 	public Text firstName;
 	public Text grade;
 	public Text stageLevel;
 	public Text points;
-	public Image reading;
-	public Image writing;
-	public Image math;
+	public Text streakLevel;
 
-	public bool DoneReading;
-	public bool DoneWriting;
-	public bool DoneMath;
+	/* To change the color of the text based on the gender */
+	public Text currentLevel;
+	public Text currentPoints;
+	public Text streakText;
+
+	public Image streak1, streak2, streak3, streak4;
+	public Image background;
+
+	public int streak;
+	public string gender;
 
 	public string[] items;
 
@@ -38,12 +44,15 @@ public class Profile : MonoBehaviour {
 		stageLevel.text = getDataValue (items [2], "stageLevel:");
 		points.text = getDataValue (items [2], "point:");
 		grade.text = getDataValue (items[2], "grade:");
-		DoneReading = Convert.ToBoolean (getDataValue (items [2], "reading:"));
-		DoneWriting = Convert.ToBoolean (getDataValue (items [2], "writing:"));
-		DoneMath = Convert.ToBoolean (getDataValue (items [2], "math:"));
-		setAccomplishment();
 
-		//Console.WriteLine (DoneReading.ToString());
+		streak = Int32.Parse (getDataValue (items[1], "streak:"));
+		streakLevel.text = (streak / 10).ToString();
+		setStreak ();
+
+		gender = getDataValue (items [2], "gender:");
+		print (getDataValue (items [1], "gender:"));
+		print (string.Equals (gender, "girl"));
+		setBackground ();
 	}
 
 	/*
@@ -60,24 +69,59 @@ public class Profile : MonoBehaviour {
 		return value;
 	}
 
-	void setAccomplishment () {
-		if(DoneReading) {
-			reading.sprite = Resources.Load<Sprite> ("done") as Sprite;
-		} else {
-			reading.sprite = Resources.Load<Sprite> ("not_done") as Sprite;
+	void setBackground () {
+		if (string.Equals (gender, "girl")) {
+			background.sprite = Resources.Load<Sprite> ("girl_background") as Sprite;
 		}
-
-		if(DoneWriting) {
-			writing.sprite = Resources.Load<Sprite> ("done") as Sprite;
-		} else {
-			writing.sprite = Resources.Load<Sprite> ("not_done") as Sprite;
-		}
-
-		if(DoneMath) {
-			math.sprite = Resources.Load<Sprite> ("done") as Sprite;
-		} else {
-			math.sprite = Resources.Load<Sprite> ("not_done") as Sprite;
+		else if (string.Equals (gender, "boy")) {
+			background.sprite = Resources.Load<Sprite> ("boy_background") as Sprite;
+			currentLevel.color = new Color(0.0f, 0.56f, 0.31f);
+			//currentPoints.color = new Color(0.46f, 1.57f, 0.06f);
+			streakText.color = Color.green;
 		}
 	}
 
+	void setStreak () {
+		switch (streak % 4) {
+		case 0:
+			setStreak (false, false, false, false);
+			break;
+		case 1:
+			setStreak (true, false, false, false);
+			break;
+		case 2:
+			setStreak (true, true, false, false);
+			break;
+		case 3:
+			setStreak (true, true, true, false);
+			break;
+		}
+
+	}
+
+	void setStreak (bool st1, bool st2, bool st3, bool st4) {
+		if (st1 == true) {
+			streak1.sprite = Resources.Load<Sprite> ("done") as Sprite;
+		} else {
+			streak1.sprite = Resources.Load<Sprite> ("not_done") as Sprite;
+		}
+
+		if (st2 == true) {
+			streak2.sprite = Resources.Load<Sprite> ("done") as Sprite;
+		} else {
+			streak2.sprite = Resources.Load<Sprite> ("not_done") as Sprite;
+		}
+
+		if (st3 == true) {
+			streak3.sprite = Resources.Load<Sprite> ("done") as Sprite;
+		} else {
+			streak3.sprite = Resources.Load<Sprite> ("not_done") as Sprite;
+		}
+
+		if (st4 == true) {
+			streak4.sprite = Resources.Load<Sprite> ("done") as Sprite;
+		} else {
+			streak4.sprite = Resources.Load<Sprite> ("not_done") as Sprite;
+		}
+	}
 }
